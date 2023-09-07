dfcaa43fb06eb9c39661abedb96f8e91e46d2c24
James Clarkson <jamesiclarkson@yahoo.co.uk>
2023-09-07 15:43:55 +1000

    IsLoggedIn fuct written

--- main.cpp
+++ main.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
+using namespace std;
+
+bool IsLoggedIn() {
+    string username, password, un, pw;
+    cout << "Enter Username: "; cin >> username;
+    cout << "Enter Password: "; cin >> password;
+
+    ifstream read("data\\" + username + ".txt");
+    getline(read, un);
+    getline(read, pw);
+
+    if (un == username && pw == password){
+        return true;
+     } else {
+        return false;
+     };
+}
+
+int Main() {
+    int choice;
+
+    cout << "1: Register\n2: Login\nYour Choice:";
+    cin >> choice;
+}
